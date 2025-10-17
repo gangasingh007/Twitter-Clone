f5003ccfe1695ea66afe8fd1e584b6e97ede4d9b
Ganga Singh <gangasingh1734@gmail.com>
2025-10-17 14:21:16 +0530

    Added the navbar Ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-import ModeToggle from "@/components/ModeToggle"
-import { Button } from "@/components/ui/button"
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
 
 function Home() {
   return (
     <div className="mt-1">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>  
-        </ SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <Button variant={"secondary"}>Click Me</Button>
-      <ModeToggle />
+     This is the home page
     </div>
   )
 }
